bbfa1ffede609b2677a2d9bfd3756f93a37b2611
imbesat-rizvi <imbugene@gmail.com>
2021-08-10 15:42:09 +0530

    Added parameters for chunk size and sample rate in AudioOutputStreamer, command line parameter to name container while executing docker_run and added MIT License

--- examples/audio.py
+++ examples/audio.py
@@ -1,13 +1,34 @@
-import rospy
-
 import sys
 from pathlib import Path
 
 path = str(Path(__file__).parents[1].resolve())
 sys.path.append(path)
 
+import argparse
+import rospy
+
 from imperio.audio import AudioOutputStreamer
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-c",
+    "--chunk_size",
+    type=int,
+    help="Specify chunk size for audio buffer",
+    default=8000,
+)
+
+parser.add_argument(
+    "-r",
+    "--sample_rate",
+    type=int,
+    help="Specify sample rate of the audio from the ROS topic",
+    default=16000,
+)
+
+args = parser.parse_args()
+
 rospy.init_node("audio_output")
-AudioOutputStreamer()
+AudioOutputStreamer(chunk_size=args.chunk_size, sample_rate=args.sample_rate)
 rospy.spin()
